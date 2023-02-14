be13cb080faad0745ecff313c96f9d3b90a008ea
Andrii Tsokur <tsokur@gmail.com>
2023-02-14 23:04:44 +0200

    Removes extra code

--- sliders/script-slider.js
+++ sliders/script-slider.js
@@ -53,24 +53,4 @@
 
       heroPicts[pictIsShown].classList.add("hero__picture--is-shown");
    }
-
-   // Slider-2
-   let slider2 = document.querySelector(".slider-2__picts");
-   //  let { left: slider2Pos } = document
-   //     .querySelector(".slider-2__picts")
-   //     .getBoundingClientRect();
-   const slider2Length =
-      document.querySelectorAll(".slider-2__picture").length - 1;
-
-   let i = 0;
-   let offset = 294;
-
-   if (++i < slider2Length) {
-      setInterval(function () {
-         slider2.style.transform = `translateX(-${offset}px)`;
-      }, 1000);
-
-      offset += 294;
-      slider2.style = "";
-   }
 })();
